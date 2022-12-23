aefa6c58fa71518254220d3a6e3caaec50368816
Karetskas_Evgeny <kub99999@yandex.ru>
2022-12-23 16:36:48 +0500

    Fixed bugs in the "WorkingWithArrays" project.
    - in the "functionForWorkingWithArrays.js" file:
    
    1. The following functions have been renamed:
    --- from "getDescendingSort" to "sortDescending";
    --- from "getEvenItemsSum" to "getEvenNumbersSum";
    --- from "getOneHundredItemsArray" to "getOneHundredNumbersArray";
    --- from "getEvenNumbersSquaresList" to "getEvenNumbersSquaresArray".
    
    2. The following functions have been created:
    --- isNotNumericArray;
    --- isArrayWithRequestedElementsCount.
    
    3. The following function have been changed:
    
    --- getStringSeparator
    (The function for dublicating the "#" symbol has been replaced.);
    
    --- sortDescending
    (The anonymous function in the "sort" method has been changed.
    The conditions for array validation have been changed to the "isNotNumericArray" function.);
    
    --- getItemsFromBeginning
    (The "filter" function has been changed to "slice".
    The conditions for array validation have been changed to the "isArrayWithRequestedElementsCount" function.);
    
    --- getItemsFromEnd
    (The "filter" function has been changed to "slice".
    The conditions for array validation have been changed to the "isArrayWithRequestedElementsCount" function.);
    
    --- getEvenNumbersSum
    (The conditions for array validation have been changed to the "isNotNumericArray" function.);
    
    --- getOneHundredNumbersArray
    (The "fill" and "map" functions have been replaced with a loop.);
    
    --- getEvenNumbersSquaresArray
    (The conditions for array validation have been changed to the "isNotNumericArray" function.).
    
    4. If the arguments of the functions do not pass validation, the function returns "null".
    
    5. The coding style has been changed.
    
    6. The "use strict" regime has been added.

--- WorkingWithArrays/functionForWorkingWithArrays.js
+++ WorkingWithArrays/functionForWorkingWithArrays.js
@@ -1,105 +1,82 @@
-﻿(function () {
-    var array = [12, 1, 11, 7, 3, 15, 2, 9, 3, 13, 10, 8, 4, 5, 2, 14, 6];
-    printToConsole(
-        "Array before sorted: [" + array.join(", ") + "].",
-        "Array after sorted: [" + getDescendingSort(array).join(", ") + "].");
+﻿"use strict";
 
-    var array1 = [12, 1, 11, 7, 3, 15, 2, 9, 3, 13, 10, 8, 4, 5, 2, 14, 6];
-    printToConsole(
-        "Current array: [" + array1.join(", ") + "].",
-        "First 5 array items: [" + getItemsFromBeginning(array1, 5).join(", ") + "].");
-
-    var array2 = [12, 1, 11, 7, 3, 15, 2, 9, 3, 13, 10, 8, 4, 5, 2, 14, 6];
-    printToConsole(
-        "Current array: [" + array2.join(", ") + "].",
-        "Last 5 array items: [" + getItemsFromEnd(array2, 5).join(", ") + "].");
-
-    var array3 = [12, 1, 11, 7, 3, 15, 2, 9, 3, 13, 10, 8, 4, 5, 2, 14, 6];
-    printToConsole(
-        "Current array: [" + array3.join(", ") + "].",
-        "Sum of even items of array: " + getEvenItemsSum(array3));
-
-    printToConsole("Array of numbers from 1 to 100 elements: [" + getOneHundredItemsArray().join(", ") + "].");
-
-    printToConsole("List of squares of even numbers: [" + getEvenNumbersSquaresList(getOneHundredItemsArray()).join(", ") + "].");
-
+(function () {
     function printToConsole() {
         console.log(getStringSeparator());
 
         var args = (arguments.length === 1 ? [arguments[0]] : Array.apply(null, arguments));
 
         args.forEach(function (item) {
-            return console.log(item);
+            console.log(item);
         });
 
         console.log(getStringSeparator());
     }
 
     function getStringSeparator() {
-        return new Array(99).fill("#").reduce(function (total, item) {
-            return total + item;
-        }, "#");
+        return "#".repeat(100);
     }
 
-    function getDescendingSort(sortableArray) {
-        if (sortableArray === null
-            || sortableArray === undefined
-            || Array.isArray(sortableArray) === false
-            || sortableArray.some((item) => typeof (item) !== "number")) {
-            return NaN;
+    function isNotNumericArray(currentArray) {
+        return Array.isArray(currentArray) === false
+            || currentArray.some(function (item) {
+                return typeof (item) !== "number";
+            });
+    }
+
+    function sortDescending(sortableArray) {
+        if (isNotNumericArray(sortableArray)) {
+            return null;
         }
 
-        return sortableArray.sort(function (element2, element1) {
-            return element1 - element2;
+        return sortableArray.sort(function (element1, element2) {
+            return element2 - element1;
         });
+    }
+
+    var array = [12, 1, 11, 7, 3, 15, 2, 9, 3, 13, 10, 8, 4, 5, 2, 14, 6];
+    printToConsole(
+        "Array before sorted: [" + array.join(", ") + "].",
+        "Array after sorted: [" + sortDescending(array).join(", ") + "].");
+
+    function isArrayWithRequestedElementsCount(currentArray, elementsCount) {
+        return Array.isArray(currentArray) === true
+            && elementsCount !== null
+            && elementsCount !== undefined
+            && typeof (elementsCount) === "number"
+            && Math.floor(elementsCount) >= 0
+            && Math.floor(elementsCount) < currentArray.length;
     }
 
     function getItemsFromBeginning(currentArray, elementsCount) {
-        if (currentArray === null
-            || currentArray === undefined
-            || Array.isArray(currentArray) === false) {
-            return NaN;
+        if (isArrayWithRequestedElementsCount(currentArray, elementsCount) === false) {
+            return null;
         }
 
-        if (elementsCount === null
-            || elementsCount === undefined
-            || typeof (elementsCount) !== "number"
-            || Math.floor(elementsCount) < 0
-            || Math.floor(elementsCount) >= currentArray.length) {
-            return NaN;
-        }
-
-        return currentArray.filter(function (item, index) {
-            return index < elementsCount;
-        });
+        return currentArray.slice(0, elementsCount);
     }
+
+    var array1 = [12, 1, 11, 7, 3, 15, 2, 9, 3, 13, 10, 8, 4, 5, 2, 14, 6];
+    printToConsole(
+        "Current array: [" + array1.join(", ") + "].",
+        "First 5 array items: [" + getItemsFromBeginning(array1, 5).join(", ") + "].");
 
     function getItemsFromEnd(currentArray, elementsCount) {
-        if (currentArray === null
-            || currentArray === undefined
-            || Array.isArray(currentArray) === false) {
-            return NaN;
+        if (isArrayWithRequestedElementsCount(currentArray, elementsCount) === false) {
+            return null;
         }
 
-        if (elementsCount === null
-            || elementsCount === undefined
-            || typeof (elementsCount) !== "number"
-            || Math.floor(elementsCount) < 0
-            || Math.floor(elementsCount) >= currentArray.length) {
-            return NaN;
-        }
-
-        return currentArray.filter(function (item, index) {
-            return index >= currentArray.length - elementsCount;
-        });
+        return currentArray.slice(currentArray.length - elementsCount);
     }
 
-    function getEvenItemsSum(currentArray) {
-        if (currentArray === null
-            || currentArray === undefined
-            || Array.isArray(currentArray) === false
-            || currentArray.some((item) => typeof (item) !== "number")) {
-            return NaN;
+    var array2 = [12, 1, 11, 7, 3, 15, 2, 9, 3, 13, 10, 8, 4, 5, 2, 14, 6];
+    printToConsole(
+        "Current array: [" + array2.join(", ") + "].",
+        "Last 5 array items: [" + getItemsFromEnd(array2, 5).join(", ") + "].");
+
+    function getEvenNumbersSum(currentArray) {
+        if (isNotNumericArray(currentArray)) {
+            return null;
         }
 
         return currentArray.filter(function (item) {
@@ -109,17 +86,34 @@
         }, 0);
     }
 
-    function getOneHundredItemsArray() {
-        return new Array(100).fill(0).map(function (item, index) {
-            return index + 1;
-        });
+    var array3 = [12, 1, 11, 7, 3, 15, 2, 9, 3, 13, 10, 8, 4, 5, 2, 14, 6];
+    printToConsole(
+        "Current array: [" + array3.join(", ") + "].",
+        "Sum of even items of array: " + getEvenNumbersSum(array3));
+
+    function getOneHundredNumbersArray() {
+        var numbersArray = [];
+
+        for (var i = 0; i < 100; i++) {
+            numbersArray.push(i + 1);
+        }
+
+        return numbersArray;
     }
 
-    function getEvenNumbersSquaresList(currentArray) {
+    printToConsole("Array of numbers from 1 to 100 elements: [" + getOneHundredNumbersArray().join(", ") + "].");
+
+    function getEvenNumbersSquaresArray(currentArray) {
+        if (isNotNumericArray(currentArray)) {
+            return null;
+        }
+
         return currentArray.filter(function (item) {
             return item % 2 === 0;
         }).map(function (item) {
             return item * item;
         });
     }
+
+    printToConsole("List of squares of even numbers: [" + getEvenNumbersSquaresArray(getOneHundredNumbersArray()).join(", ") + "].");
 })();
