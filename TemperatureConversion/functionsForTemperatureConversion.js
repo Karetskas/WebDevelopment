25ab3ad1248223d2b6de24c14b14cf1ec298e8c1
Karetskas_Evgeny <kub99999@yandex.ru>
2023-01-09 19:19:53 +0500

    Fixed bugs in the "TemperatureConversion" project.
    - The following files have been renamed:
    --- from "functionForTemperatureConvertion.js" to "functionForTemperatureConversion.js";
    --- from "temperatureConvertionStyleSheet.css" to "temperatureConversionStyleSheet.css".
    
    - Change in the "temperatureConversionHtmlPage.html" file:
    1. The "href="temperatureConvertionStyleSheet.css"" attribute in the "link" tag has been changed to "href="temperatureConversionStyleSheet.css"".
    2. The "src="functionsForTemperatureConvertion.js"" attribute in the "script" tag has been changed to "src="functionsForTemperatureConversion.js"".
    3. The follow classes attributes have been renamed:
    --- from "class="fahrenheit_conversion_button"" to "class="button"";
    --- from "class="kelvin_conversion_button"" to "class="button"".
    4. The "value="-34.15"" attribute in the input tag has been change to "value="0"".
    5. The "h2" tag has been added.
    
    - Change in the "temperatureConversionStyleSheet.css" file:
    1. The following selectors have been renamed:
    --- from ".fahrenheit_conversion_button, .kelvin_conversion_button" to ".button";
    --- from ".kelvin_conversion_button" to ".button:hover";
    --- from ".typeResult, .result" to ".temperature_in_celsius, .result".
    --- from ".typeResult" to ".temperature_in_celsius".
    2. The ".result_history" selector has been added.
    3. The following selectors have been removed:
    --- ".result";
    --- ".label_control".
    4. The "@media (max-width: 341px)" rule has been change to "@media (max-width: 400px)".
    5. The "width: 185px;" property in the ".button" selector has been added.
    6. The "width: 35%;" in the ".temperature_in_celsius, .result" selector has been changed to "width: 49%".
    
    - Change in the "functionForTemperatureConversion.js" file:
    1. The "isGreen" variable has been removed.
    2. The following functions have been renamed:
    --- from "checkTemperaturekeyboardInput" to "isCorrectTemperature";
    --- from "getColorToHex" to "getHexColor";
    --- from "setColorForElements" to "setElementsColor";
    --- from "outputToScreen" to "convertTemperature".
    3. The logic of the "isCorrectTemperature" function has been changed.
    4. The following variables and arguments in the "getElementWithResult" function have been change:
    --- from "name" to "temperatureInCelsius";
    --- from "divTypeResult" to "divTemperatureInCelsius".
    5. The code has been formatted in the "getHexColor" function.
    6. The "temperature" argument in the "getTemperatureInFahrenheit" and "getTemperatureInKelvin" functions have been added.
    7. The "temperatureInputField" and "currentCorrectTemperature" variables have been moved into the "convertTemperature" function.
    8. The following variables in the "convertTemperature" function have been added:
    --- temperatureCelsiusText;
    --- resultHistory.
    9. The method of forming a subscription to button click events has been changed.
    10. All code has been wrapped in the "DOMContentLoaded" event.

---
+++ TemperatureConversion/functionsForTemperatureConversion.js
@@ -0,0 +1,138 @@
+﻿"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+    function isCorrectTemperature(temperature, textFieldElement) {
+        if (temperature !== "" && temperature.length <= 15 && !isNaN(temperature)) {
+            return true;
+        }
+
+        textFieldElement.style.backgroundColor = "#E00";
+
+        var timerId = setInterval(function () {
+            textFieldElement.value = "0";
+            textFieldElement.style.backgroundColor = "#0B0";
+        }, 1000);
+
+        setTimeout(function () {
+            clearInterval(timerId);
+        }, 1010);
+
+        return false;
+    }
+
+    function getElementsWithResult(temperatureInCelsius, calculationResult) {
+        var divContainer = document.createElement("div");
+        divContainer.classList.add("flex_container_for_result");
+
+        var divTemperatureInCelsius = document.createElement("div");
+        divTemperatureInCelsius.innerHTML = temperatureInCelsius;
+        divTemperatureInCelsius.classList.add("temperature_in_celsius");
+
+        var divWithResult = document.createElement("div");
+        divWithResult.innerHTML = calculationResult;
+        divWithResult.classList.add("result");
+
+        divContainer.append(divTemperatureInCelsius);
+        divContainer.append(divWithResult);
+
+        return divContainer;
+    }
+
+    function getHexColor(color) {
+        var colorsArray = color.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+
+        return (colorsArray && colorsArray.length === 4)
+            ? "#" +
+                ("0" + parseInt(colorsArray[1], 10).toString(16)).slice(-2) +
+                ("0" + parseInt(colorsArray[2], 10).toString(16)).slice(-2) +
+                ("0" + parseInt(colorsArray[3], 10).toString(16)).slice(-2)
+            : NaN;
+    }
+
+    function setElementsColor(containersWithResults) {
+        if (containersWithResults === null
+            || containersWithResults === undefined
+            || {}.toString.call(containersWithResults) !== "[object NodeList]"
+            || containersWithResults.length === 0) {
+            return;
+        }
+
+        var computedStyleForTypeResult = getComputedStyle(containersWithResults[0].querySelector(".temperature_in_celsius"));
+
+        var elementBackgroundColor = computedStyleForTypeResult.backgroundColor;
+
+        var color = parseInt(getHexColor(elementBackgroundColor).slice(1), 16);
+
+        for (var i = 0; i < containersWithResults.length; i++) {
+            var typeResultElement = containersWithResults[i].querySelector(".temperature_in_celsius");
+
+            var hexadecimalNumberString = color.toString(16);
+
+            typeResultElement.style.backgroundColor = "#00" + hexadecimalNumberString;
+
+            var result = containersWithResults[i].querySelector(".result");
+
+            result.style.backgroundColor = "#00" + hexadecimalNumberString;
+
+            color -= 13000;
+        }
+    }
+
+    function getTemperatureInFahrenheit(temperature) {
+        var result = 1.8 * parseFloat(temperature) + 32;
+
+        return Math.round(result * 100) / 100;
+    }
+
+    function getTemperatureInKelvin(temperature) {
+        var result = parseFloat(temperature) + 273.15;
+
+        return Math.round(result * 100) / 100;
+    }
+
+    var elementsWithResultCount = 0;
+
+    function convertTemperature(event) {
+        var temperatureInputField = document.querySelector(".text_box_control");
+        var currentCorrectTemperature = temperatureInputField.value.trim();
+
+        if (!isCorrectTemperature(currentCorrectTemperature, temperatureInputField)) {
+            return;
+        }
+
+        var calculationResult;
+        var elementWithResult;
+
+        var temperatureCelsiusText = "Celsius" + "<br />" + currentCorrectTemperature;
+
+        if (event.currentTarget.name === "conversion_to_degrees_fahrenheit") {
+            calculationResult = getTemperatureInFahrenheit(currentCorrectTemperature);
+
+            elementWithResult = getElementsWithResult(temperatureCelsiusText, "Fahrenheit" + "<br />" + calculationResult);
+        } else {
+            calculationResult = getTemperatureInKelvin(currentCorrectTemperature);
+
+            elementWithResult = getElementsWithResult(temperatureCelsiusText, "Kelvin" + "<br />" + calculationResult);
+        }
+
+        var resultHistory = document.querySelector(".result_history");
+        resultHistory.textContent = "History of result:";
+        resultHistory.after(elementWithResult);
+
+        elementsWithResultCount++;
+
+        var containersWithResultsCollection = document.querySelectorAll(".flex_container_for_result");
+
+        setElementsColor(containersWithResultsCollection);
+
+        if (elementsWithResultCount > 5) {
+            containersWithResultsCollection[containersWithResultsCollection.length - 1].remove();
+        }
+    }
+
+    var buttons = document.querySelectorAll(".button");
+
+    buttons.forEach(function (button) {
+        button.addEventListener("click", convertTemperature);
+    });
+});
