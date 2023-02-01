e76507d465e16d16baf6b77b681e1b512a1d5388
Karetskas_Evgeny <kub99999@yandex.ru>
2023-02-01 19:43:51 +0500

    Fixed bugs in the "PhoneBookWithJQuery" project.

--- PhoneBookWithJQuery/javaScriptForPhoneBook.js
+++ PhoneBookWithJQuery/javaScriptForPhoneBook.js
@@ -1,57 +1,87 @@
 ﻿"use strict";
 
 $(document).ready(function () {
+    function showDialog(event) {
+        var tableRow = $(event.target).closest(".table_row");
+
+        var lastName = tableRow.find(".last_name").text();
+        var firstName = tableRow.find(".first_name").text();
+        var phoneNumber = tableRow.find(".phone_number").text();
+
+        $(".dialog .last_name").text(lastName);
+        $(".dialog .first_name").text(firstName);
+        $(".dialog .phone_number").text(phoneNumber);
+
+        $(".dialog").dialog({
+            resizable: false,
+            height: "auto",
+            width: 280,
+            modal: true,
+            show: {
+                effect: "slide",
+                duration: 500
+            },
+            hide: {
+                effect: "slide",
+                duration: 500
+            },
+            buttons: {
+                "Delete": function () {
+                    deleteContactInTable(event);
+                    $(this).dialog("close");
+                },
+                Cancel: function () {
+                    $(this).dialog("close");
+                }
+            }
+        });
+    }
+
     function deleteContactInTable(event) {
         $(event.target).off();
 
-        var rowContainer = $(event.target).closest(".table_row");
+        var row = $(event.target).closest(".table_row");
 
-        rowContainer.remove();
+        row.remove();
 
-        var rowsTable = $(".table_row");
+        var tableRows = $(".table_row");
 
-        if (rowsTable.length === 0) {
+        if (tableRows.length === 0) {
             $(".heading_no_contacts").show();
 
             return;
         }
 
-        var elementIndex = $(".table_row").index(rowContainer);
+        var rowToDeleteIndex = tableRows.index(row);
 
-        for (var i = elementIndex; i < rowsTable.length; i++) {
-            rowsTable.eq(i).find("span:first").text(i + 1);
+        for (var i = rowToDeleteIndex; i < tableRows.length; i++) {
+            tableRows.eq(i).find("div:first").text(i + 1);
         }
     }
 
     function getNewContactRow(contactObject) {
-        var tableRows = $(".table_row");
-
-        var contactsCount = tableRows.length + 1;
+        var contactsCount = $(".table_row").length + 1;
 
         var rowContainer = $("<div class=\"table_row\"></div>");
 
         rowContainer
             .append($("<div></div>")
-                .append($("<span></span")
-                    .text(contactsCount)));
+                .text(contactsCount));
 
         rowContainer
-            .append($("<div></div>")
-                .append($("<span></span>")
-                    .text(contactObject.lastName)));
+            .append($("<div class=\"last_name\"></div>")
+                .text(contactObject.lastName));
 
         rowContainer
-            .append($("<div></div>")
-                .append($("<span></span>")
-                    .text(contactObject.firstName)));
+            .append($("<div class=\"first_name\"></div>")
+                .text(contactObject.firstName));
 
         rowContainer
-            .append($("<div></div>")
-                .append($("<span></span>")
-                    .text(contactObject.phoneNumber)));
+            .append($("<div class=\"phone_number\"></div>")
+                .text(contactObject.phoneNumber));
 
-        var rowDeleteButton = $("<input class=\"delete_row\" type=\"button\" name=\"delete_contact\" value=\"delete\" />")
-            .on("click", deleteContactInTable);
+        var rowDeleteButton = $("<input class=\"delete_row\" type=\"button\" name=\"delete_contact\" value=\"X\" title=\"Delete this row\" />")
+            .click(showDialog);
 
         rowContainer
             .append($("<div></div>")
@@ -60,26 +90,13 @@ $(document).ready(function () {
         return rowContainer;
     }
 
-    function hideTooltip(wrapper) {
-        wrapper.removeClass("invalid_input_in_text_box");
+    function hideErrorMessage(wrapper) {
+        wrapper.removeClass("invalid")
+            .val("");
 
         wrapper.next()
             .hide()
             .text("");
-    }
-
-    function clearFormToAddContact() {
-        var textBoxes = $(".text_box");
-
-        var addContactButton = $(".adding_contact_form>.container_row:last>input");
-        addContactButton.prop("disabled", true);
-        addContactButton.removeClass("add_contact_button_enabled");
-        addContactButton.addClass("add_contact_button_disabled");
-
-        textBoxes.each(function () {
-            $(this).val("");
-            hideTooltip($(this));
-        });
     }
 
     function addContactToTable() {
@@ -91,7 +108,9 @@ $(document).ready(function () {
             phoneNumber: textBoxes.eq(2).val()
         });
 
-        clearFormToAddContact();
+        $(".adding_contact_form>.container_row:last>input").prop("disabled", true);
+
+        hideErrorMessage($(".text_box"));
 
         if ($(".table_row").length === 0) {
             $(".heading_no_contacts").hide();
@@ -102,86 +121,129 @@ $(document).ready(function () {
         textBoxes.eq(0).focus();
     }
 
-    $(".adding_contact_form>.container_row:last>input").on("click", addContactToTable);
+    $(".adding_contact_form>.container_row:last>input").click(addContactToTable);
 
-    function showTooltip(wrapper, message, isValid) {
-        var tooltip = wrapper.next();
+    function showErrorMessage(wrapper, message, isValid) {
+        var errorMessage = wrapper.next();
 
-        tooltip.addClass("tooltip_successful_validation");
-        wrapper.removeClass("invalid_input_in_text_box");
+        errorMessage.addClass("error_message_successful_validation");
+        wrapper.removeClass("invalid");
 
         if (!isValid) {
-            tooltip.removeClass("tooltip_successful_validation");
-            wrapper.addClass("invalid_input_in_text_box");
+            errorMessage.removeClass("error_message_successful_validation");
+            wrapper.addClass("invalid");
         }
 
-        tooltip
+        errorMessage
             .show()
             .html(message);
     }
 
-    function validateTextBox(event) {
-        var text = $(event.target).val();
+    function isPhoneBookNumber(phoneNumber) {
+        var phoneNumbers = $(".table .phone_number");
 
-        var tooltips = "";
-
-        if (text.length === 0) {
-            tooltips = "Required field!<br/>";
+        if (phoneNumbers.length === 0) {
+            return false;
         }
 
-        if (text.length > 255) {
-            tooltips += "Text longer than 255 characters!<br/>";
-        }
-
-        if (text.length !== 0 && text[0] === " ") {
-            tooltips += "Space at the beginning of the line!<br/>";
-        }
-
-        if (text.length !== 0 && text.length !== 1 && text[text.length - 1] === " ") {
-            tooltips += "Space at the end of the line!<br/>";
-        }
-
-        if ($(event.target).prop("type") === "tel") {
-            if (!/[0-9]+/.test(text)) {
-                tooltips += "The phone number must contain at least 1 digit!<br/>";
-            }
-
-            if (/[^0-9-+.() ]/.test(text)) {
-                tooltips += "A phone number can contain: (, ), +, -, numbers, dots and spaces!<br/>";
+        for (var i = 0; i < phoneNumbers.length; i++) {
+            if (phoneNumbers.eq(i).text() === phoneNumber) {
+                return true;
             }
         }
 
+        return false;
+    }
+
+    function accessTelephoneBook(event, errorMessages) {
         var addContactButton = $(".adding_contact_form>.container_row:last>input");
 
-        if (tooltips !== "") {
-            tooltips = tooltips.slice(0, tooltips.lastIndexOf("<br/>"));
+        if (errorMessages !== "") {
+            errorMessages = errorMessages.slice(0, errorMessages.lastIndexOf("<br/>"));
 
-            showTooltip($(event.target), tooltips, false);
+            showErrorMessage($(event.target), errorMessages, false);
 
             addContactButton.prop("disabled", true);
-            addContactButton.removeClass("add_contact_button_enabled");
-            addContactButton.addClass("add_contact_button_disabled");
 
             return;
         }
 
-        tooltips = "This field is correct!";
-        showTooltip($(event.target), tooltips, true);
+        errorMessages = "This field is correct!";
+        showErrorMessage($(event.target), errorMessages, true);
 
         var textBoxes = $(".text_box");
 
         for (var i = 0; i < textBoxes.length; i++) {
-            if (textBoxes.eq(i).hasClass("invalid_input_in_text_box") || textBoxes.eq(i).val() === "") {
+            if (textBoxes.eq(i).hasClass("invalid") || textBoxes.eq(i).val() === "") {
                 return;
             }
         }
 
         addContactButton.prop("disabled", false);
-        addContactButton.removeClass("add_contact_button_disabled");
-        addContactButton.addClass("add_contact_button_enabled");
     }
 
-    $(".text_box").each(function () {
-        $(this).on("input", validateTextBox);
-    });
+    var timerId = null;
+    var isTimerStarted = false;
+
+    function validateTextBox(event) {
+        var text = $(event.target).val();
+
+        var errorMessages = "";
+
+        if (text.length === 0) {
+            errorMessages = "Required field!<br/>";
+        }
+
+        if (text.length > 255) {
+            errorMessages += "Text longer than 255 characters!<br/>";
+        }
+
+        if (text.length !== 0 && text[0] === " ") {
+            errorMessages += "Space at the beginning of the line!<br/>";
+        }
+
+        if (text.length !== 0 && text.length !== 1 && text[text.length - 1] === " ") {
+            errorMessages += "Space at the end of the line!<br/>";
+        }
+
+        if ($(event.target).prop("type") === "tel") {
+            if (!/[0-9]+/.test(text)) {
+                errorMessages += "The phone number must contain at least 1 digit!<br/>";
+            }
+
+            if (/[^0-9-+.() ]/.test(text)) {
+                errorMessages += "A phone number can contain: (, ), +, -, numbers, dots and spaces!<br/>";
+            }
+
+            if (errorMessages.length === 0) {
+                accessTelephoneBook(event, "Search for matches in the directory...");
+
+                isTimerStarted = true;
+
+                clearTimeout(timerId);
+
+                timerId = setTimeout(function () {
+                    if (text.trim() !== "") {
+                        if (isPhoneBookNumber(text)) {
+                            errorMessages += "The phone number is already in the phone book!<br/>";
+                        } else {
+                            errorMessages += "";
+                        }
+
+                        accessTelephoneBook(event, errorMessages);
+
+                        isTimerStarted = false;
+
+                        return;
+                    }
+                }, 1000);
+            }
+        }
+
+        if (!isTimerStarted) {
+            accessTelephoneBook(event, errorMessages);
+        }
+    }
+
+    $(".text_box").on("input", validateTextBox);
 });
