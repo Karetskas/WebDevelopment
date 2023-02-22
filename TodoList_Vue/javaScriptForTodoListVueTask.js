c75d9242b234152e635a84388eb11e1fa888ec5f
Karetskas_Evgeny <kub99999@yandex.ru>
2023-02-22 15:57:38 +0500

    Fixed bugs in the "TodoList_Vue" project.

---
+++ TodoList_Vue/javaScriptForTodoListVueTask.js
@@ -0,0 +1,90 @@
+﻿new Vue({
+    el: "#app",
+
+    data: {
+        nextTaskId: 1,
+        tasks: [],
+        taskText: "",
+        isValidText: true
+    },
+
+    watch: {
+        taskText: function (text) {
+            if (text.length !== 0) {
+                this.isValidText = true;
+            }
+        }
+    },
+
+    mounted: function () {
+        this.setFocusForNewTask();
+    },
+
+    methods: {
+        addTask: function () {
+            this.setFocusForNewTask();
+
+            if (this.taskText.length === 0) {
+                this.isValidText = false;
+
+                return;
+            }
+
+            this.tasks.push({
+                id: this.nextTaskId,
+                text: this.taskText,
+                editTaskText: this.taskText,
+                isEditMode: false,
+                isValidText: true
+            });
+
+            this.taskText = "";
+            this.nextTaskId++;
+        },
+
+        removeTask: function (taskIndex) {
+            this.tasks.splice(taskIndex, 1);
+        },
+
+        startEditTask: function (task) {
+            task.editTaskText = task.text;
+
+            task.isEditMode = true;
+
+            this.setFocusForEditTask(task);
+        },
+
+        cancelEditTask: function (task) {
+            task.isEditMode = false;
+        },
+
+        saveTask: function (task) {
+            if (task.editTaskText.length === 0) {
+                task.isValidText = false;
+
+                this.setFocusForEditTask(task);
+
+                return;
+            }
+
+            task.text = task.editTaskText;
+            task.isEditMode = false;
+        },
+
+        checkTextForValidity: function (task) {
+            if (task.editTaskText.length !== 0) {
+                task.isValidText = true;
+            }
+        },
+
+        setFocusForNewTask: function () {
+            this.$refs.enterTask.focus();
+        },
+
+        setFocusForEditTask: function (task) {
+            this.$nextTick(function () {
+                this.$refs[task.id][0].focus();
+            });
+        }
+    }
+});
